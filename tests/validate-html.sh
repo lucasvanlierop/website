5d65b0091302a07f7d7345cf51c865eea7c09625
Lucas van Lierop <lucas@vanlierop.org>
2016-12-10 21:36:52 +0000

    Catch only stderr output for validation

--- tests/validate-html.sh
+++ tests/validate-html.sh
@@ -2,18 +2,18 @@
 
 set -x
 
-VALIDATE_OUTPUT=$(
+VALIDATE_ERRORS=$(
     docker run \
     --rm \
     --name=html-validator \
     --network=container:lucasvanlierop-website-nginx \
     magnetikonline/html5validator \
     java -jar /root/build/validator.nu/vnu.jar http://nginx \
-    2>&1
+    2>&1 > /dev/null
 )
 
 # Catch warnings too
-if [[ ! -z "${VALIDATE_OUTPUT}" ]]; then
-    echo "HTML NOT VALID ${VALIDATE_OUTPUT}"
+if [[ ! -z "${VALIDATE_ERRORS}" ]]; then
+    echo "HTML NOT VALID ${VALIDATE_ERRORS}"
     exit 1
 fi
