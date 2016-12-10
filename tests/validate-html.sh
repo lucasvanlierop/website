58a27afca16a5a4a8cd062af234508eac2b4576e
Lucas van Lierop <lucas@vanlierop.org>
2016-12-10 21:50:07 +0000

    Store stderr in variable while outputting stdout

--- tests/validate-html.sh
+++ tests/validate-html.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-set -x
-
-VALIDATE_ERRORS=$(
+# Note that in order to capture the commands stderr its' redirected to stdout
+# While stdout itself is redirected to a third filedescriptor which is outputted later on.
+{ VALIDATE_ERRORS=$(
     docker run \
     --rm \
     --name=html-validator \
     --network=container:lucasvanlierop-website-nginx \
     magnetikonline/html5validator \
     java -jar /root/build/validator.nu/vnu.jar http://nginx \
-    2>&1 > /dev/null
-)
+    2>&1 1>&3-
+) ;} 3>&1
 
 # Catch warnings too
 if [[ ! -z "${VALIDATE_ERRORS}" ]]; then
