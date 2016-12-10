4269ccc9534ff4c78b82b2ec031663d3f802ea50
Lucas van Lierop <lucas@vanlierop.org>
2016-12-10 15:45:23 +0000

    Also fail on HTML validate warnings

--- tests/validate-html.sh
+++ tests/validate-html.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-docker run \
+VALIDATE_OUTPUT=$(
+    docker run \
     --rm \
     --name=html-validator \
     --network=container:lucasvanlierop-website-nginx \
     magnetikonline/html5validator \
-    java -jar /root/build/validator.nu/vnu.jar http://nginx
+    java -jar /root/build/validator.nu/vnu.jar http://nginx \
+    2>&1
+)
+
+# Catch warnings too
+if [[ ! -z "${VALIDATE_OUTPUT}" ]]; then
+    echo "HTML NOT VALID ${VALIDATE_OUTPUT}"
+    exit 1
+fi
