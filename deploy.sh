07e01f2c808cf7e330eeba6091290e49b6f5fbae
Lucas van Lierop <lucas@vanlierop.org>
2016-12-12 09:49:07 +0000

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+scp -r docker-compose.yml deploy@lucasvanlierop.nl:/home/deploy
+ssh deploy@lucasvanlierop.nl <<COMMANDS
+    docker-compose up -d --force-recreate --no-build --remove-orphans
+COMMANDS
