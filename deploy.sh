0c9247561a26ca15d2062c22fdc6af8697803f4c
Lucas van Lierop <lucas@vanlierop.org>
2016-12-13 08:40:40 +0000

    Always pull latest image on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 scp -r docker-compose.yml deploy@lucasvanlierop.nl:/home/deploy
 ssh deploy@lucasvanlierop.nl <<COMMANDS
+    docker-compose pull
     docker-compose up -d --force-recreate --no-build --remove-orphans
 COMMANDS
